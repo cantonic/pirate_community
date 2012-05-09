ae5e827d0c6bebc24382e87f9f42787c8affe79a
Can Tonic <can@cantonic.com>
2012-05-09 18:54:14 +0300

    and another capistrano fix

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 server 'pp-staging.cantonic.com', :app, :web, :primary => true
 set :branch, "develop"
-set :deploy_to, "/home/apps/#{application}_staging"
+set :deploy_to, "/home/deployer/apps/#{application}_staging"
 
 default_environment["PATH"]         = "/usr/local/rvm/gems/ruby-1.9.3-p194@pirate_community_staging/bin:/usr/local/rvm/gems/ruby-1.9.3-p194@global/bin:/usr/local/rvm/rubies/ruby-1.9.3-p194/bin:/usr/local/rvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
 default_environment["GEM_HOME"]     = "/usr/local/rvm/gems/ruby-1.9.3-p194@pirate_community_staging"
