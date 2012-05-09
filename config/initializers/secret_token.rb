37500b37d09cc16036615e351fa553a0d186b311
Can Tonic <can@cantonic.com>
2012-05-09 14:23:12 +0300

    app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PirateCommunity::Application.config.secret_token = '8212a4d768c8d803684970ae8a9342dc5b58dde79063ff33051eaf7b6108fb383e113f256bd70b383daeecac7c9d5e5973e6f1cbf647697b9fd01f1d39a343eb'
